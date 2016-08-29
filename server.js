6c099d63547025596454e83eba7a5b89d098dec9
huyuguo <huyuguook@163.com>
2016-08-29 14:14:57 +0800

    打印信息修改

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var users = exports.users = [];
 var log4js = require('log4js');
 log4js.configure({
   appenders:[
-    // {type: 'console'},
+    {type: 'console'},
     {
       type: 'file',
       filename: 'logs/normal.log',
